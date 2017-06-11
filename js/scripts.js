434b808caa189cfcc3d541cf281975e8139722de
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-06-10 19:11:15 -0700

    some progress in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
 //business logic
 var vowels = ["a", "e", "i", "o", "u"];
 
+function isVowel(char) {
+  if (vowels.indexOf(char.toLowerCase()) === -1) {
+    return false;
+  }
+  return true;
+}
 
-var pigLatin = function(inputtedString) {
+function startWithVowel(word) {
+  var firstChar = word[0];
+  return isVowel(firstChar);
+}
 
-  var firstLetter = inputtedString.charAt(0);
-  var pigString = inputtedString.slice(1) + firstLetter + "ay"; //consonant case
-
-  vowels.forEach(function(vowel) {
-    if (vowel === firstLetter) {
-      pigString = inputtedString + "way"; //vowel case
+function pigLatin(word) {
+  if (startWithVowel(word)) {
+    return word + "way";
+  } else {
+    var suffix = "";
+    for (i in word) {
+      if (!isVowel(word[i])) {
+        suffix = suffix + word[i];
+      } else if(word[i].toLowerCase())
     }
-  });
-
-  return pigString;
-};
-
-
-
-
+    return word;
+  }
+}
 
 //user interface logic
 $(document).ready(function() {
